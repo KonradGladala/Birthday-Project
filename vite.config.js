3ff95eae7bc26ae3e63da0e48eca2be468eecc24
MdxKG545 <43971594+KonradGladala@users.noreply.github.com>
2025-06-15 00:23:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 export default {
+  base: '/', // This is the corrected line
+  root: '.',
+  server: {
+    open: true
+  }
+};
+
+/*
+export default {
     base: '/Birthday-Project/', // e.g. '/bunny-rpg/',
     root: '.',
     server: {
       open: true
     }
-  };
+  };*/
